d31f8479d8b22c33712720e4258197d7e0c8011c
Michael Zeisel <mhzeisel@gmail.com>
2022-02-09 22:25:59 -0500

    Update moving

---
+++ restapi_practice/maintest.py
@@ -0,0 +1,18 @@
+from operator import pos
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+def hello():
+    return {"Hello": "World"}
+
+@app.post('/')
+def hello_post():
+    return {"Success": "You posted!"}
+
+@app.get('/something')
+def something():
+    return {"Data": "Something"}
